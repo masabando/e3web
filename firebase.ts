e7bddacd784d697c520d344d3569ea9d08cbefbe
Masamitsu Bando <bando@ktc.ac.jp>
2026-06-02 19:17:18 +0900

    feat: Firebaseの初期化ロジックを更新
    getAppsを使用して、既存のFirebaseアプリがある場合はそれを再利用するように変更しました。

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 export { auth, googleProvider };
